80b6f8fc1be9a1a718de315c1039d714c7fb5040
anhphha <74095848+anhphha@users.noreply.github.com>
2022-04-13 04:14:18 +0300

    Revert "PA deployed to Heroku"
    This reverts commit 5dc3a8974242ee4fb914f5948be57d45470398e1.

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 
 API_KEY = os.environ["API_KEY"]
 BASE_URL = os.environ["BASE_URL"]
-user_input = "helsinki"
+user_input = input("Enter city: ")
 LAT = 60.1699
 LON = 24.9384
 
@@ -50,5 +50,4 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    FLASK_APP.run(debug =  True, host="0.0.0.0", port = port)
+    app.run(host='localhost', port=5000, debug=True)
