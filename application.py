79f3b4b7f84d4d0d9a0d80492825c428a74517f3
anhphha <74095848+anhphha@users.noreply.github.com>
2022-04-21 04:38:13 +0300

    modified files for AWS deployment

--- application.py
+++ application.py
@@ -1,8 +1,4 @@
 import requests
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 from flask import Flask, render_template
 import os
 
@@ -12,32 +8,18 @@ user_input = "helsinki"
 LAT = 60.1699
 LON = 24.9384
 
-FLASK_APP = Flask(__name__, static_url_path="",
+application=Flask(__name__, static_url_path="",
                   static_folder="web/static", template_folder="web/templates")
-<<<<<<< HEAD
-=======
 
->>>>>>> master
-
-@FLASK_APP.route('/')
+@application.route('/')
 
 def hello_world():
-<<<<<<< HEAD
-    return render_template('index.html', message="Hello World! ")
-=======
-
     return render_template('index.html', message="Hello World! ")
 
->>>>>>> master
-
-@FLASK_APP.route('/current')
+@application.route('/current')
 
 def current():
     try:
-<<<<<<< HEAD
-=======
-
->>>>>>> master
         r = requests.get(
             f'{BASE_URL}/weather?q={user_input}&units=metrics&appid={API_KEY}')
         r.raise_for_status()
@@ -55,28 +37,18 @@ def current():
     except requests.exceptions.HTTPError as err:
         return f"Error: {err}"
 
-@FLASK_APP.route('/forecast')
-def forecast():
-<<<<<<< HEAD
-=======
+@application.route('/forecast')
 
->>>>>>> master
+def forecast():
     r = requests.get(f'{BASE_URL}/onecall?lat={LAT}&lon={LON}&units=metrics&appid={API_KEY}').json()
     return render_template("index.html", forecast= r["daily"])
 
-@FLASK_APP.errorhandler(404)
-<<<<<<< HEAD
+@application.errorhandler(404)
 
-=======
->>>>>>> master
 def page_not_found(error):
     return render_template("index.html", message=error), 404
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-
-=======
->>>>>>> master
     port = int(os.environ.get("PORT", 5000))
-    FLASK_APP.run(debug =  True, host="0.0.0.0", port = port)
+    application.run(debug =  True, host="0.0.0.0", port = port)
